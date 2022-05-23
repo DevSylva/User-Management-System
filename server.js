5b37ec3b7b8888e66ddcddcd54665b5a2bff7804
sylva <sylvaejike@gmail.com>
2022-05-23 04:24:42 +0100

    'omo'

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const bodyparser = require("body-parser");
+const path = require("path");
 
+const connectDB = require("./server/database/connection");
 
 const app = express();
-dotenv.config()
-const PORT  = process.env.PORT || 8000
+dotenv.config();
+const PORT = process.env.PORT || 8000;
 
-app.get('/', (req, res) => {
-    res.send("Crud Application")
-})
+// log requests
+app.use(morgan("tiny"));
+// parse request to the body-parser
+app.use(bodyparser.urlencoded({ extended: true }));
 
+// set view engine
+app.set("view engine", "ejs");
+// app.set("views", path.resolve(__dirname, "views/ejs"))
+
+// load assests
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")));
+app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
+app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
+
+// load routers
+app.use('/', require('./server/routes/router'));
 app.listen(3000, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-})
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
